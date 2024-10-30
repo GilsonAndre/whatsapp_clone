bad9b68772f27a3cee80beb94cf560e18db6bebf
GilsonAndre <gilsonandre1999@gmail.com>
2024-10-30 20:17:56 -0300

    strings created

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/firebase_options.dart';
+import 'package:whatsapp_clone/resources/strings.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '',
+      title: Strings.appName,
     );
   }
 }
---
+++ lib/resources/strings.dart
@@ -0,0 +1,4 @@
+class Strings {
+  static const appName = 'Whatsapp Clone';
+  
+}
