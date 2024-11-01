cccbaadf501a425e557f37e87b758cfd1ed7e9b2
GilsonAndre <gilsonandre1999@gmail.com>
2024-10-31 23:15:58 -0300

    validate for email and password

--- lib/view/login_page.dart
+++ lib/view/login_page.dart
@@ -3,67 +3,95 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:whatsapp_clone/resources/strings.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  LoginPage({super.key});
+
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: Column(
-              children: [
-                SizedBox(height: 100.h),
-                Icon(Icons.message),
-                SizedBox(height: 30.h),
-                Text(
-                  Strings.welcomeBack,
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
-                SizedBox(height: 20.h),
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: Strings.email,
+      body: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Column(
+                children: [
+                  SizedBox(height: 100.h),
+                  Icon(Icons.message),
+                  SizedBox(height: 30.h),
+                  Text(
+                    Strings.welcomeBack,
+                    style: Theme.of(context).textTheme.titleMedium,
                   ),
-                ),
-                SizedBox(height: 10.h),
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: Strings.password,
+                  SizedBox(height: 20.h),
+
+                  //EMAIL
+                  TextFormField(
+                    autocorrect: true,
+                    decoration: InputDecoration(
+                      hintText: Strings.email,
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Email can\'t be empty';
+                      }
+                      return null;
+                    },
                   ),
-                ),
-                SizedBox(height: 25.h),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  height: 50.h,
-                  child: OutlinedButton(
-                    onPressed: () {},
-                    child: Text(
-                      Strings.login,
-                      style: Theme.of(context).textTheme.titleSmall,
+                  SizedBox(height: 10.h),
+
+                  //PASSWORD
+                  TextFormField(
+                    autocorrect: true,
+                    decoration: InputDecoration(
+                      hintText: Strings.password,
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Password can\'t be empty';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 25.h),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: 50.h,
+                    child: OutlinedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          print('VALID');
+                        } else {
+                          print('INVALID');
+                        }
+                      },
+                      child: Text(
+                        Strings.login,
+                        style: Theme.of(context).textTheme.titleSmall,
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(height: 15.h),
-                
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      Strings.notMember,
-                      style: Theme.of(context).textTheme.titleMedium,
-                    ),
-                    SizedBox(
-                      width: 5.w,
-                    ),
-                    Text(
-                      Strings.registerNow,
-                      style: Theme.of(context).textTheme.bodyLarge,
-                    ),
-                  ],
-                ),
-              ],
+                  SizedBox(height: 15.h),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        Strings.notMember,
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                      SizedBox(
+                        width: 5.w,
+                      ),
+                      Text(
+                        Strings.registerNow,
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
